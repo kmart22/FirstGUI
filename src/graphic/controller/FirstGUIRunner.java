8c7c6bed3a57837298dab87b320187e877cdab05
kmart22 <kmar7070@740s-pl-111.local>
2016-11-01 07:58:06 -0600

    created repo for FirstGUI

---
+++ src/graphic/controller/FirstGUIRunner.java
@@ -0,0 +1,12 @@
+package graphic.controller;
+
+public class FirstGUIRunner
+{
+
+	public static void main (String [] args)
+	{
+		FirstGUIController FirstGUIController = new FirstGUIController();
+		FirstGUIController.start();
+	}
+	
+}
