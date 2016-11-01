feb7d26e112705593b878ac213a0276b97dd1eda
kmart22 <kmar7070@740s-pl-111.local>
2016-11-01 09:27:46 -0600

    added layout method and place to put all generated code

--- src/graphic/view/FirstPanel.java
+++ src/graphic/view/FirstPanel.java
@@ -3,6 +3,7 @@ package graphic.view;
 import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.SpringLayout;
 import graphic.controller.FirstGUIController;
 
 import java.awt.event.ActionListener;
@@ -13,6 +14,7 @@ public class FirstPanel extends JPanel
 	private FirstGUIController baseController;
 	private JButton colorButton;
 	private JButton randomButton;
+	private SpringLayout  baseLayout;
 
 	public FirstPanel(FirstGUIController baseController)
 	{
@@ -21,7 +23,10 @@ public class FirstPanel extends JPanel
 
 		colorButton = new JButton("Color!");
 		randomButton = new JButton("Random");
-
+		baseLayout = new SpringLayout();
+		
+		
+		
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -29,6 +34,7 @@ public class FirstPanel extends JPanel
 
 	private void setupPanel()
 	{
+		this.setLayout(baseLayout);
 		this.setBackground(Color.cyan);
 		this.add(colorButton);
 		this.add(randomButton);
@@ -36,7 +42,10 @@ public class FirstPanel extends JPanel
 
 	private void setupLayout()
 	{
-
+		baseLayout.putConstraint(SpringLayout.SOUTH, colorButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, colorButton, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, randomButton, 0, SpringLayout.EAST, colorButton);
 	}
 
 	private void setupListeners()
